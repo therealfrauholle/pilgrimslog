6a137241f1d88667fd5ffa29def1c56c73c81d6b
Finn Hermeling <markushermeling@gmx.de>
2025-01-17 14:53:41 +0100

    chore: remove backend folder

--- backend/src/api/blog-entry/controllers/blog-entry.ts
+++
@@ -1,7 +0,0 @@
-/**
- * blog-entry controller
- */
-
-import { factories } from '@strapi/strapi'
-
-export default factories.createCoreController('api::blog-entry.blog-entry');
--- backend/src/api/blog-entry/routes/blog-entry.ts
+++
@@ -1,7 +0,0 @@
-/**
- * blog-entry router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::blog-entry.blog-entry');
