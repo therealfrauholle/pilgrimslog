9c58bf2e6b4c5d0a03ebf81ffe3dba737e183927
danielwl1 <67736435+danielwl1@users.noreply.github.com>
2025-02-01 21:31:34 +0100

    fix: slider updates correctly

--- src/components/CustomSlider.tsx
+++ src/components/CustomSlider.tsx
@@ -1,8 +1,9 @@
 import { ILogEntries, ILogEntry } from '@/services/FetchService';
 import { BookPageIndex } from '@/types/BookPageIndex';
-import { Slider } from '@base-ui-components/react/slider';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
 
 export default function CustomSlider({
     entries,
@@ -16,39 +17,38 @@ export default function CustomSlider({
     const dayIndex = entries.data.findIndex(
         (entry) => entry.Id == currentlySelectedDay.Id,
     );
-    if (index != dayIndex) {
-        console.log('update index to ', dayIndex);
+
+    useEffect(() => {
         setIndex(dayIndex);
-    }
+    }, [dayIndex]); 
+
     console.log('Slider sees index', dayIndex);
+
+    const handleChange = (event: Event, newValue: number | number[]) => {
+        setIndex(newValue as number);
+        console.log('value updated', newValue);
+    };
+
     return (
         <div className="flex grid place-content-center">
-            <Slider.Root
-                value={index}
-                step={1}
-                min={0}
-                max={entries.data.length - 1}
-                onValueChange={(value) => {
-                    console.log('value updated', value);
-                }}
-                onValueCommitted={(value) => {
-                    router.push(
-                        BookPageIndex.entry(
-                            entries.data[value as number],
-                        ).asUrl(),
-                    );
-                }}
-            >
-                <Slider.Control className="flex w-56 items-center py-3">
-                    <Slider.Track className="h-1 w-full rounded bg-gray-500 shadow-[inset_0_0_0_1px] shadow-gray-500">
-                        <Slider.Indicator className="rounded bg-gray-700" />
-                        <Slider.Thumb
-                            aria-valuetext={index + ''}
-                            className="h-[20px] w-[10px] bg-white outline outline-1 outline-gray-300 focus-visible:outline focus-visible:outline-2 focus-visible:outline-blue-800"
-                        />
-                    </Slider.Track>
-                </Slider.Control>
-            </Slider.Root>
+            <Box sx={{ width: 300 }}>
+                <Slider
+                    value={index}
+                    step={1}
+                    min={0}
+                    max={entries.data.length - 1}
+                    onChange={handleChange}
+                    onChangeCommitted={(event, value) => {
+                        router.push(
+                            BookPageIndex.entry(
+                                entries.data[value as number],
+                            ).asUrl(),
+                        );
+                    }}
+                >
+                </Slider>
+            </Box>
         </div>
     );
 }
+
