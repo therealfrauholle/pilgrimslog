42eb77e4d6ecfeb9fb2a18987f265b6ac7074564
danielwl1 <67736435+danielwl1@users.noreply.github.com>
2025-02-02 02:13:14 +0100

    WIP

--- src/components/CustomSlider.tsx
+++ src/components/CustomSlider.tsx
@@ -4,6 +4,26 @@ import { useRouter } from 'next/navigation';
 import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import Slider from '@mui/material/Slider';
+import { SliderThumb } from '@mui/material/Slider';
+import { ZoomIn } from '@mui/icons-material';
+import React from 'react';
+
+
+const myThumb: React.ElementType = (props) => {
+
+
+    return (
+        <SliderThumb
+            {...props}
+            onPointerDown={(e) => {
+                props.onPointerDown?.(e); // Preserve default behavior
+                // handlePointerDown(); // Add custom behavior
+            }}
+        >
+        </SliderThumb>
+    );
+}
+
 
 export default function CustomSlider({
     entries,
@@ -14,40 +34,63 @@ export default function CustomSlider({
 }) {
     const router = useRouter();
     const [index, setIndex] = useState(0);
+    const [isZoomed, setIsZoomed] = useState(false);
     const dayIndex = entries.data.findIndex(
         (entry) => entry.Id == currentlySelectedDay.Id,
     );
 
     useEffect(() => {
         setIndex(dayIndex);
-    }, [dayIndex]); 
+    }, [dayIndex]);
 
     console.log('Slider sees index', dayIndex);
 
     const handleChange = (event: Event, newValue: number | number[]) => {
         setIndex(newValue as number);
         console.log('value updated', newValue);
+        setIsZoomed(true);
     };
 
+    function handlePointerDown() {
+        setIsZoomed(true);
+    }
+
+
+    let smallTrack = 300;
+    let bigDick = 500;
+    let percentalOffset = index / entries.data.length;
+    let offsetCenter = 0.5 - percentalOffset;
+    let maximumAbsoluteOffset = (bigDick - smallTrack);
+    let marginRight = offsetCenter * maximumAbsoluteOffset;
+
+
     return (
-        <div className="flex grid place-content-center">
-            <Box sx={{ width: 300 }}>
+        <div className="relative h-[100px] overflow-x-hidden w-full min-w-0 pt-[50px]">
+            <div className={'absolute left-[50%] translate-x-[-50%] transition-[width] min-w-0 place-self-center duration-300 ease-in-out ' + (isZoomed ? 'w-[500px]' : 'w-[300px]') + ' h-[50px]'}
+            style= {isZoomed ? {paddingRight: marginRight*2} : {}}
+            >
                 <Slider
                     value={index}
                     step={1}
                     min={0}
                     max={entries.data.length - 1}
                     onChange={handleChange}
+                    marks={true}
                     onChangeCommitted={(event, value) => {
                         router.push(
                             BookPageIndex.entry(
                                 entries.data[value as number],
                             ).asUrl(),
                         );
+                        setIsZoomed(false);
                     }}
+                    slots={{ thumb: myThumb }}
+                    slotProps={{ thumb: { onPointerDown: handlePointerDown } }}
+                    valueLabelDisplay="auto"
+                    valueLabelFormat={(value) => value}
                 >
                 </Slider>
-            </Box>
+            </div>
         </div>
     );
 }
