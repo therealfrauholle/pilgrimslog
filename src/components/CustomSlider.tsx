95023f8fe56e855d5a1867f20eaf5f8bcaad6644
Finn Hermeling <markushermeling@gmx.de>
2025-02-02 13:39:40 +0100

    attempt to implement smoooth zoom algorithm

--- src/components/CustomSlider.tsx
+++ src/components/CustomSlider.tsx
@@ -1,11 +1,9 @@
 import { ILogEntries, ILogEntry } from '@/services/FetchService';
 import { BookPageIndex } from '@/types/BookPageIndex';
 import { useRouter } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import Box from '@mui/material/Box';
+import { useEffect, useRef, useState } from 'react';
 import Slider from '@mui/material/Slider';
 import { SliderThumb } from '@mui/material/Slider';
-import { ZoomIn } from '@mui/icons-material';
 import React from 'react';
 
 const myThumb: React.ElementType = (props) => {
@@ -20,6 +18,14 @@ const myThumb: React.ElementType = (props) => {
     );
 };
 
+enum ZoomState {
+    Normal,
+    Zooming,
+    ZoomingWithChange,
+    Zoomed,
+    Release,
+}
+
 export default function CustomSlider({
     entries,
     currentlySelectedDay,
@@ -29,7 +35,8 @@ export default function CustomSlider({
 }) {
     const router = useRouter();
     const [index, setIndex] = useState(0);
-    const [isZoomed, setIsZoomed] = useState(false);
+    const [zoomState, setZoomState] = useState(ZoomState.Normal);
+    const containerRef = useRef<HTMLDivElement>(null);
     const dayIndex = entries.data.findIndex(
         (entry) => entry.Id == currentlySelectedDay.Id,
     );
@@ -41,13 +48,15 @@ export default function CustomSlider({
     console.log('Slider sees index', dayIndex);
 
     const handleChange = (event: Event, newValue: number | number[]) => {
-        setIndex(newValue as number);
         console.log('value updated', newValue);
-        setIsZoomed(true);
+        if (newValue != index && zoomState != ZoomState.Zoomed) {
+            setZoomState(ZoomState.ZoomingWithChange);
+        }
+        setIndex(newValue as number);
     };
 
     function handlePointerDown() {
-        setIsZoomed(true);
+        setZoomState(ZoomState.Zooming);
     }
 
     const normalWidth = 300;
@@ -55,19 +64,50 @@ export default function CustomSlider({
     const percentalOffset = index / entries.data.length;
     const offsetCenter = 0.5 - percentalOffset;
     const maximumAbsoluteOffset = zoomedWidth - normalWidth;
-    const marginRight = offsetCenter * maximumAbsoluteOffset;
-
+    const spacingLeft = offsetCenter * maximumAbsoluteOffset;
+    let spacingString = '+ ' + spacingLeft + 'px';
+    if (spacingLeft < 0) {
+        spacingString = '- ' + -spacingLeft + 'px';
+    }
+    console.log('spaxing ought to be ' + spacingString);
+    const isZoomed = [
+        ZoomState.ZoomingWithChange,
+        ZoomState.Zooming,
+        ZoomState.Zoomed,
+    ].includes(zoomState);
     return (
         <div className="relative h-[100px] overflow-x-hidden w-full min-w-0 pt-[50px]">
             <div
+                onTransitionEnd={() => {
+                    if (zoomState == ZoomState.Release) {
+                        console.log('transition ended, state normal');
+                        setZoomState(ZoomState.Normal);
+                    } else {
+                        console.log('transition ended, state zoomed');
+                        setZoomState(ZoomState.Zoomed);
+                    }
+                }}
+                ref={containerRef}
                 className={
-                    'absolute left-[50%] translate-x-[-50%] transition-[width] min-w-0 place-self-center duration-300 ease-in-out ' +
+                    'absolute translate-x-[-50%] transition-[left,width] min-w-0 place-self-center duration-[1s] ease-in-out ' +
+                    ([
+                        ZoomState.Zooming,
+                        ZoomState.Normal,
+                        ZoomState.Release,
+                    ].includes(zoomState)
+                        ? 'duration-[1s] transition-[left,width]'
+                        : zoomState == ZoomState.ZoomingWithChange
+                            ? 'duration-100 transition-[left,width]'
+                            : 'transition-none') +
+                    ' ' +
                     (isZoomed
                         ? 'w-[' + zoomedWidth + 'px]'
-                        : 'w-[' + normalWidth + '300px]') +
+                        : 'w-[' + normalWidth + 'px]') +
                     ' h-[50px]'
                 }
-                style={isZoomed ? { paddingRight: marginRight * 2 } : {}}
+                style={{
+                    left: isZoomed ? 'calc(50% ' + spacingString + ')' : '50%',
+                }}
             >
                 <Slider
                     value={index}
@@ -82,7 +122,7 @@ export default function CustomSlider({
                                 entries.data[value as number],
                             ).asUrl(),
                         );
-                        setIsZoomed(false);
+                        setZoomState(ZoomState.Release);
                     }}
                     slots={{ thumb: myThumb }}
                     slotProps={{ thumb: { onPointerDown: handlePointerDown } }}
