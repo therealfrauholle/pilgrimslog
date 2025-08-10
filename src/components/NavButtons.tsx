befd935d0b6f162155541040a87b1b0549fe4bce
danielwl1 <danielwl1@outlook.de>
2025-08-10 23:43:26 +0200

    hotfix: declared fixed size to navigation arrow icons

---
+++ src/components/NavButtons.tsx
@@ -0,0 +1,62 @@
+import { ArrowBack, ArrowForward } from '@mui/icons-material';
+import { BookPageIndex } from '@/util/BookPageIndex';
+
+type NavigationButtonsProps = {
+    currentIndex: BookPageIndex;
+    onNavigate: (index: BookPageIndex) => void;
+};
+
+export function NavigationButtons({ currentIndex, onNavigate }: NavigationButtonsProps) {
+    return (
+        <>
+            <div
+                className="nav-button"
+                style={{
+                    left: '0%',
+                    zIndex: 2000,
+                    opacity: currentIndex.navPrev() ? 1 : 0,
+                }}
+                onClick={() => {
+                    const previous = currentIndex.navPrev();
+                    if (previous) {
+                        onNavigate(previous);
+                    }
+                }}
+                role="button"
+            >
+                <ArrowBack
+                    sx={{
+                        fontSize: '36px',
+                        fill: '#303030',
+                        stroke: '#FEF4EC',
+                        strokeWidth: 0.5,
+                    }}
+                />
+            </div>
+            <div
+                className="nav-button"
+                style={{
+                    right: '0%',
+                    zIndex: 2000,
+                    opacity: currentIndex.navNext() ? 1 : 0,
+                }}
+                onClick={() => {
+                    const next = currentIndex.navNext();
+                    if (next) {
+                        onNavigate(next);
+                    }
+                }}
+                role="button"
+            >
+                <ArrowForward
+                    sx={{
+                        fontSize: '36px',
+                        fill: '#303030',
+                        stroke: '#FEF4EC',
+                        strokeWidth: 0.5, 
+                    }}
+                />
+            </div>
+        </>
+    );
+}
